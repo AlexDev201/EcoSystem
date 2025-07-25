68a25af88cf51aa10fead71084e2e44a873a4f36
Giovanny Molina <maicol3285@gmail.com>
2025-07-24 22:27:39 -0500

    Integragracion CRUD basica completa

--- backend/eco_energy/src/main/java/com/ecoenergy/eco_energy/device/controller/DeviceController.java
+++ backend/eco_energy/src/main/java/com/ecoenergy/eco_energy/device/controller/DeviceController.java
@@ -2,11 +2,13 @@ package com.ecoenergy.eco_energy.device.controller;
 
 import com.ecoenergy.eco_energy.device.model.Device;
 import com.ecoenergy.eco_energy.device.service.DeviceService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,6 +28,39 @@ public class DeviceController {
         return deviceService.getDevice(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
-
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Device> updateDevice(@PathVariable UUID id, @RequestBody Device updatedData) {
+        return deviceService.getDevice(id)
+                .map(existingDevice -> {
+                    existingDevice.setName(updatedData.getName());
+                    existingDevice.setType(updatedData.getType());
+
+                    // Asignamos ACTIVE hasta que el usuario lo cambie
+                    Device.DeviceStatus status = updatedData.getStatus() != null
+                            ? updatedData.getStatus()
+                            : Device.DeviceStatus.ACTIVE;
+                    existingDevice.setStatus(status);
+
+                    existingDevice.setUpdatedAt(LocalDateTime.now());
+
+                    Device updatedDevice = deviceService.updateDevice(existingDevice);
+                    return ResponseEntity.ok(updatedDevice);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteDevice(@PathVariable UUID id) {
+        if (deviceService.getDevice(id).isEmpty()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        deviceService.deleteDevice(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
 }
